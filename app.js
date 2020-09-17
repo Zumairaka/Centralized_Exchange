633fee1aef81f13986d71ac3a4a19b46f30f0d9f
Zumairaka <zumairaka@gmail.com>
2020-09-17 14:14:09 +0530

    intermediate

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var createError = require('http-errors');
+const cors = require('cors');
+const bodyparser = require('body-parser');
 var Web3 = require('web3');
 var mongoose = require('mongoose');
 
@@ -35,15 +34,12 @@ db.once('open',()=>{
     console.log("Success");
 });
 
-// setup view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  }));
 
 //routers
 app.use('/register', registerRouter);
@@ -52,17 +48,7 @@ app.use('/createBTCNode', createBTCNodeRouter);
 app.use('/createETHNode', createETHNodeRouter);
 app.use('/clientCurrency', clientCurrencyRouter);
 
-// error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Listeing to Port: 3000");
 });
-
-app.use(function(err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
